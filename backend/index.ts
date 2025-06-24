c7318d35d764372131dcccb6b3cccc6944a56b85
Felipe <nunesfb@gmail.com>
2025-06-24 15:39:56 -0300

    deploy

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,10 @@
+// backend/index.ts
 import serverless from "serverless-http";
 import app from "./src/app";
+import { connectDatabase } from "./src/config/database";
+
+// Inicia a conexão uma vez em cold start
+connectDatabase();
 
 export const config = { maxDuration: 60 };
 const handler = serverless(app);
