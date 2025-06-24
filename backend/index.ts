214ceca4152cb63eea028dcb3ed7c8b304ed4b3c
Felipe <nunesfb@gmail.com>
2025-06-24 15:36:05 -0300

    ajuste rasp

--- backend/index.ts
+++ backend/index.ts
@@ -1,25 +1,6 @@
-// backend/index.ts
-import dotenv from "dotenv";
 import serverless from "serverless-http";
 import app from "./src/app";
-import { connectDatabase } from "./src/config/database";
 
-dotenv.config();
-
-// Conecta uma vez no cold start
-const dbConnect = connectDatabase();
-// Cria o handler Express → Serverless
-const lambda = serverless(app);
-
-// **Default export** da função que vai ser chamada
-export default async function handler(req: any, res: any) {
-  try {
-    await dbConnect;
-    return lambda(req, res);
-  } catch (err: any) {
-    console.error("❌ Fatal error in handler:", err);
-    res
-      .status(500)
-      .json({ message: "Internal server error", details: err.message });
-  }
-}
+export const config = { maxDuration: 60 };
+const handler = serverless(app);
+export default handler;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,35 +1,25 @@
-// src/app.ts
+// backend/src/app.ts
+
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import addressRoutes from "./routes/address.routes";
 import projectRoutes from "./routes/project.routes";
 import userProjectRoutes from "./routes/userProject.routes";
 
-dotenv.config();
-
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
+
 app.use("/users", userRoutes);
 app.use("/addresses", addressRoutes);
 app.use("/projects", projectRoutes);
 app.use("/user-projects", userProjectRoutes);
 
-// Middleware genérico de tratamento de erros (opcional)
-app.use(
-  (
-    err: any,
-    _req: express.Request,
-    res: express.Response,
-    _next: express.NextFunction
-  ) => {
-    console.error(err);
-    res.status(500).json({ message: "Erro interno", details: err.message });
-  }
-);
+// Middleware genérico de tratamento de erros
+app.use((err: any, _req: any, res: any, _next: any) => {
+  console.error(err);
+  res.status(500).json({ message: "Erro interno", details: err.message });
+});
 
 export default app;
